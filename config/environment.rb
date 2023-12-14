8a16f8a8987e4135f11769efbbf2a7707a6f6cc5
kwame bio <kwameagyemang73@gmail.com>
2023-12-14 00:08:52 +0000

    setup mailer configurations

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Setup ActionMailer
+ActionMailer::Base.delivery_method = :smtp
+
+ActionMailer::Base.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "gmail.com",
+  user_name: ENV.fetch("GMAIL_USERNAME", nil),
+  password: ENV.fetch("GMAIL_APP_PASSWORD", nil),
+  authentication: "plain",
+  enable_starttls_auto: true
+}
