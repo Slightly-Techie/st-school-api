281aa7fcdc0fd0031029e5ed3e93c847e97d2e4d
kwame bio <kwameagyemang73@gmail.com>
2023-12-14 17:24:41 +0000

    clean up

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,8 @@
 class UserMailer < ApplicationMailer
+    default from: "slightlytechie@gmail.com"
     def reset_password_email
         @user = params[:user]
-        @url = "#{ENV.fetch('EMAIL_URL', 'slightlytechie')}/reset-password/#{@user[:reset_password_token]}"
-        mail(to: @user.email, subject: "Reset your password")
+        @url = "#{ENV.fetch('EMAIL_URL', 'http://localhost:6002')}/reset-password/#{@user[:reset_password_token]}"
+        mail(to: @user.email, subject: "Reset your password", from: "slightlytechie@gmail.com")
     end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  host = "http://localhost:6002"
+  config.action_mailer.default_url_options = { host: host }
 end
